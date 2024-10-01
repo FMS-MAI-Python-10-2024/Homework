fec65095fb78f930f2e93f57e5352db610b9595c
Nyamerka <okteamail@gmail.com>
2024-10-01 11:47:13 +0300

    Fix tests2

--- test_homework/tests/test_variant_0.py
+++ test_homework/tests/test_variant_0.py
@@ -12,6 +12,8 @@ class TestVariant0(unittest.TestCase):
         if not os.path.exists(solution_file):
             self.fail(f"Solution file not found: {solution_file}")
 
+        print(f"Solution file found: {solution_file}, running tests...")
+
         spec = importlib.util.spec_from_file_location(student_name, solution_file)
         student_solution_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(student_solution_module)
@@ -29,7 +31,10 @@ class TestVariant0(unittest.TestCase):
         for input_data, expected_output in test_cases:
             with patch('builtins.input', side_effect=input_data), patch('builtins.print') as mock_print:
                 try:
-                    exec(open(solution_file).read())
+                    if hasattr(student_solution_module, 'student_solution'):
+                        student_solution_module.student_solution()
+                    else:
+                        exec(open(solution_file).read())
                     mock_print.assert_called_with(expected_output)
                     success_count += 1
                 except AssertionError:
@@ -50,8 +55,33 @@ class TestVariant0(unittest.TestCase):
         if student_file in os.listdir(tasks_folder):
             self.run_tests_for_student(student_name)
         else:
-            self.skipTest(f"Skipped tests for {student_name}: not this variant.")
+            self.fail(f"Test skipped: no file {student_file} found in tasks folder.")
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+# a, b = map(int, input().split())
+# summ = a + b
+#
+# if summ <= 10:
+#     print("Пикай Хорька!")
+# elif 10 < summ <= 100:
+#     print("Каждый дуб когда был жёлудём...")
+# elif 100 < summ <= 1000:
+#     print("One shot - one kill.")
+# else:
+#     print("Хафизов нехороший человек.")
+
+# a, b = map(int, input().split())
+# summ = a + b
+#
+# if summ <= 10:
+#     print("Пdghfdgfикай Хорька!")
+# elif 10 < summ <= 100:
+#     print("Кажdgfdjgfjдый дуб когда был жёлудём...")
+# elif 100 < summ <= 1000:
+#     print("Ondgfdjgfe shot - one kill.")
+# else:
+#     print("Хафизовdjfdjh нехороший человек.")
