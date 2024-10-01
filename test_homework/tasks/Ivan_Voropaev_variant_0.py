6f79cac97d4f1cbe2b56e9e6b48e8d9eebcb7570
Nyamerka <okteamail@gmail.com>
2024-10-01 15:22:09 +0300

    Try to send solution

--- test_homework/tasks/Ivan_Voropaev_variant_0.py
+++ test_homework/tasks/Ivan_Voropaev_variant_0.py
@@ -1,11 +1,11 @@
-a, b = map(int, input().split())
-summ = a + b
-
-if summ <= 10:
-    print("Пикай Хорька!")
-elif 10 < summ <= 100:
-    print("Каждый дуб когда был жёлудём...")
-elif 100 < summ <= 1000:
-    print("One shot - one kill.")
-else:
-    print("Хафизов нехороший человек.")
+def student_solution():
+    a, b = map(int, input().split())
+    summ = a + b
+    if summ <= 10:
+        print("Пикай Хорька!")
+    elif 10 < summ <= 100:
+        print("Каждый дуб когда был жёлудём...")
+    elif 100 < summ <= 1000:
+        print("One shot - one kill.")
+    else:
+        print("Хафизов нехороший человек.")
